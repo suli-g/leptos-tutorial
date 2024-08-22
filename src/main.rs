8156f0ee85746d2e9a241ccb2c80b2eed6169a4f
Sulaiman Gafoor <5022721+suli-g@users.noreply.github.com>
2024-08-22 21:51:39 +0200

    Create reactive Leptos component

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 use leptos::*;
 
+#[component]  // Function arguments are available as a props struct.
+fn App() -> impl IntoView {
+    let (count , set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                set_count(3);
+            }
+        >
+            "Click me: "
+            {move || count()}
+        </button>
+    }
+}
+
 fn main() {
     console_error_panic_hook::set_once();
-    mount_to_body(|| view! { <p>"Hello, world!"</p> });
+    mount_to_body(|| view! { <App /> });
 }
