593bba7fcae4c4abcd0819583c41be0ba416d9ce
Sulaiman Gafoor <5022721+suli-g@users.noreply.github.com>
2024-08-23 15:30:35 +0200

    Implement guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use leptos::*;
+use rand::Rng;
 
 /*
 Adds a progress bar with a maximum count of 100.
 */
 #[component]
 fn ProgressBar(
-    #[prop(default=100)] // Replace default with 'optional' if no default needed.
+    #[prop(default = 100)] // Replace default with 'optional' if no default needed.
     max: u16,
     progress: ReadSignal<i32>,
 ) -> impl IntoView {
@@ -23,23 +24,41 @@ be manipulated:
  */
 #[component]
 fn App() -> impl IntoView {
+    let mut rng = rand::thread_rng();
+    let max_value = 20;
     let (count, set_count) = create_signal(0);
+    let guess: i32 = rng.gen_range(0..max_value);
     view! {
         <ProgressBar
             progress=count
         />
-        <button
-            on:click=move |_| {
-                set_count.update(|n| *n += 1)
-            }
-            class:red=move || count() % 2 == 1
-            class=("blue-21", move || count() % 3 == 1)  // Changes the class based on the value of count.
+        <div
+            class="counter"
+            class=("blue-21", move || count() == guess)  // Changes the class based on the value of count.
             // Change to style based on the value of count:
-            style:background-color=move || format!("rgba({}, 0, 100, 0.{})", count(), count()) // dynamic attribute
+            style=move || format!("border-radius: calc(var(--base-radius) + {0}px) ; background-color: rgba(0, calc({0} * 25), 0, calc({0} / 100))", <i32>::abs(count() - guess))
+            style:border=move || if count() == guess { "1px solid red" } else {"none"}
         >
-            "Click me: "
-            {count} // Identical to {move || count()}
+        <h1
+        
+        >{count}</h1> // Identical to {move || count()}
+        </div>
+        <fieldset class="buttons">
+        <button
+        on:click=move |_| {
+            set_count.update(|n| *n += 1)
+        }
+        >
+        "+"
         </button>
+        <button
+        on:click=move |_| {
+            set_count.update(|n| *n -= 1)
+        }
+        >
+        "-"
+        </button>
+        </fieldset>
     }
 }
 
